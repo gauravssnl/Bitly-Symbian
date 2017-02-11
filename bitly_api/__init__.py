6a06226b77138ce2015ab7c6918c6ffa6e24af3f
GAURAV SINGH <Gaurav.ssnl@gmail.com>
2017-02-12 05:23:04 +0530

    Add files via upload

--- bitly_api/__init__.py
+++ bitly_api/__init__.py
@@ -1,1 +1,11 @@
+from __future__ import absolute_import
+from bitly_api.bitly_api import Connection, BitlyError, Error
+__version__ = '0.3'
+__author__ = "Jehiah Czebotar <jehiah@gmail.com>"
+__all__ = ["Connection", "BitlyError", "Error"]
+__doc__ = """
+This is a python library for the bitly api
 
+all methods raise BitlyError on an unexpected response, or a problem with input
+format
+"""
